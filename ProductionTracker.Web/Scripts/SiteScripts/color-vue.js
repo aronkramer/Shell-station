d4af3f67913272efb5488e75f40ba4628c3a351c
aron kramer <aronkramer123@gmail.com>
2019-06-05 12:04:15 -0400

    changing colors to vue.js file (adds row to color adder)

---
+++ ProductionTracker.Web/Scripts/SiteScripts/color-vue.js
@@ -0,0 +1,12 @@
+﻿var app = new Vue({
+    el: '#app',
+    data: {
+        formColors: [{ Id: null, Name: null }],
+        test:'hello'
+    },
+    methods: {
+        addRowtoColorForm: function () {
+            this.formColors.push({ Id: null, Name: null })
+        }
+    }
+})
