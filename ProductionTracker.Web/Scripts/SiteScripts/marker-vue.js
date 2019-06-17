b64d8d4115dbdfb9f756bf2aa9c432aefd1a9c30
Barry Hauser <barryhauser@gmail.com>
2019-06-17 16:41:23 -0400

    Added update functions and delete

--- ProductionTracker.Web/Scripts/SiteScripts/marker-vue.js
+++ ProductionTracker.Web/Scripts/SiteScripts/marker-vue.js
@@ -19,10 +19,12 @@
             $.get("/marker/GetMarkeCatsWithMarkers", result => {
                 this.markerCatergories = result.map(r => {
                     r.DefaltMarker.SizeText = this.markerSizeText(r.DefaltMarker.MarkerDetails);
-                    r.DefaltMarker.CreatedOn = moment(r.DefaltMarker.CreatedOn).format('MM/DD/YYYY');
+                    //r.DefaltMarker.CreatedOn = moment(r.DefaltMarker.CreatedOn).format('MM/DD/YYYY');
                     r.Markers = r.Markers.map(m => {
                         m.SizeText = this.markerSizeText(m.MarkerDetails);
-                        m.CreatedOn = moment(m.CreatedOn).format('MM/DD/YYYY');
+                        //m.CreatedOn = moment(m.CreatedOn).format('MM/DD/YYYY');
+                        m.Copy = null;
+                        m.Edit = false;
                         return m;
                     });
                     r.DetailsOpened = false;
@@ -31,8 +33,60 @@
 
                 });
             });
+        },
+        edit: function (markerCatIndex, MarkerIndex) {
+            var marker = this.markerCatergories[markerCatIndex].Markers[MarkerIndex];
+            if (!marker.Edit) {
+                this.editCancelMarker(true, markerCatIndex, MarkerIndex);
+            }
+        },
+        cancel: function (markerCatIndex, MarkerIndex) {
+            this.editCancelMarker(false, markerCatIndex, MarkerIndex);
+        },
+        editCancelMarker: function (edit, markerCatIndex, MarkerIndex) {
+            var marker = this.markerCatergories[markerCatIndex].Markers[MarkerIndex];
+            
+            marker.Edit = edit;
+            if (edit) {
+                marker.Copy = jQuery.extend(true, {}, marker);
+            }
+            else {
+                marker.Length = marker.Copy.Length;
+                marker.PercentWaste = marker.Copy.PercentWaste;
+            }
+            this.markerCatergories[markerCatIndex].Markers.splice(MarkerIndex, 1, marker);
+        },
+        updateMarker: function (markerCatIndex, MarkerIndex) {
+            var marker = this.markerCatergories[markerCatIndex].Markers[MarkerIndex];
+            this.updateMarkerInDb({ Id: marker.Id, Length: marker.Length, PercentWaste: marker.PercentWaste },
+                () => {
+                    marker.Edit = false;
+                    this.markerCatergories[markerCatIndex].Markers.splice(MarkerIndex, 1, marker);
+
+                });
+        },
+        deleteMarker: function (markerCatIndex, MarkerIndex) {
+            if (confirm("Your sure you want to delete this?!")) {
+                var marker = this.markerCatergories[markerCatIndex].Markers[MarkerIndex];
+                this.updateMarkerInDb({ Id: marker.Id, Deleted: true }, () => {
+                    this.markerCatergories[markerCatIndex].Markers.splice(MarkerIndex, 1);
+
+                });
+            }
+        },
+        updateMarkerInDb: function (marker, func) {
+            $.post('/marker/UpdateMarker', { marker }, () => { if (func) func(); });
         }
+        
 
 
+    },
+    filters: {
+        percentage: function (value) {
+            return value ? (value * 100).toFixed(2) + '%' : '0%';
+        },
+        toShortDateString: function (value) {
+            return moment(value).format('MM/DD/YYYY');
+        }
     }
 });
