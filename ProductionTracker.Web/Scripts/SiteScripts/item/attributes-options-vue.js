a154de2a0ebd52fa36a78d76814c4d379f029f49
aron kramer <aronkramer123@gmail.com>
2019-06-27 15:19:54 -0400

    1) Made a partial view for the color page (called itemAttribute) 2) color.html has a selection box for more editing stuff

---
+++ ProductionTracker.Web/Scripts/SiteScripts/item/attributes-options-vue.js
@@ -0,0 +1,15 @@
+﻿var appnew = new Vue({
+    el: '#app-other-edit-options',
+    mounted: function () {
+        console.log('kdjsuhfklj');
+    },
+    data: {
+        test:'hello world'
+    },
+    methods: {
+        addRowtoColorForm: function () {
+            this.formColors.push({ Id: null, Name: null })
+        },
+    
+    }
+})
---
+++ ProductionTracker.Web/Scripts/SiteScripts/item/color-vue.js
@@ -0,0 +1,60 @@
+﻿var app = new Vue({
+    el: '#app',
+    mounted: function () {
+        this.getColors();
+    },
+    data: {
+        formColors: [{ Id: null, Name: null }],
+        listOfAllColors: []
+    },
+    methods: {
+        addRowtoColorForm: function () {
+            this.formColors.push({ Id: null, Name: null })
+        },
+        submitColors: function () {
+            $.post("/item/AddColors", { colors: this.formColors }, () => {
+                this.getColors();
+                //this.listOfAllColors = this.listOfAllColors.concat(this.formColors);
+                this.formColors = [{ Id: null, Name: null }];
+            })
+        },
+        getColors: function (func) {
+            var x = $.get("/item/GetColors", result => {
+                this.listOfAllColors = result.map(r => {
+                    r.Edit = false;
+                    r.NameCopy = null;
+                    return r;
+                });
+            })
+            console.log(x.responseJSON);
+        },
+        updateExistingColor: function (index) {
+
+            var color = this.listOfAllColors[index];
+            //{ color: { Id: color.Id, Name: color.Name } }
+            $.post('/item/EditColors', { ColorId: color.Id, ColorName: color.Name }, () => {
+                //this.listOfAllColors[index].NameCopy = this.listOfAllColors[index].Name;
+            });
+            
+        },
+        deleteAnExistingItem: function (index) {
+            swal({
+                title: "Are you sure you want to delete this?",
+                text: "Your will not be able to recover!",
+                type: "warning",
+                showCancelButton: true,
+                confirmButtonClass: "btn-danger",
+                confirmButtonText: "Yes, delete it!",
+                showLoaderOnConfirm: true,
+                closeOnConfirm: true
+            },
+                () => {
+                    var row = this.listOfAllColors[index];
+                    $.post('/item/DeleteColors', { ColorId: row.Id }, () => {
+                        this.listOfAllColors.splice(index,1);
+                    });
+                });
+           
+        },
+    }
+})
